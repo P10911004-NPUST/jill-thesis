0c9fc40b0622a72b3f42536473ccfb205acb35ef
jklai.npust <jklai.npust@gmail.com>
2023-06-26 17:16:37 +0800

    setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# jill-thesis
+# Jill-thesis
 GUI for Jill-Tsai
+
+## Notation:
+- The **GDAL** and **Fiona** packages could not be downloaded directly via **pip install**. 
+- They are currently downloaded with **pipwin install**: pip install **pipwin** and **wheel** packages first.
+- After the **GDAL** and **Fiona** were successfully installed, then pip install **geopandas** and **rioxarray**, otherwise error might be raised, especially when installing **geopandas**.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import subprocess
+from pathlib import Path
+
+pyenv_dir = Path(sys.executable).parent.parent
+pkgs = os.listdir(os.path.join(pyenv_dir, "Lib", "site-packages"))
+
+def pipwin_install(pkgs_name):
+    if [i for i in pkgs if i.startswith(pkgs_name) is None]:
+        subprocess.check_call([sys.executable, '-m', 'pipwin', 'install', pkgs_name])
+    else:
+        print(f"{pkgs_name} already exists.")
+
+# implement pip as a subprocess:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pipwin', 'wheel'])
+pipwin_install("GDAL")
+pipwin_install("Fiona")
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'geopandas', 'rioxarray'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pycaret[full]', 'streamlit'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'opencv-contrib-python', 'scikit-image'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
